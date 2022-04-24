e875dc6ebd4456a17a2d8e311bfd5e41e4243d03
nada <nada.mohamed@extech.com>
2022-04-25 00:42:00 +0200

    seed admins, users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Check email
+        $user = User::where('name', $fields['name'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return back()->with('error', 'Bad credentials');
+        }
+
+        if (Auth::guard('user')->attempt(['name' => request('name'),
+            'password' => request('password')])) {
+
+            return redirect()->route('task.create')
+                ->with('success', 'Logged In Successfully');
+
+        } else {
+            return back()->with('error', 'Bad credentials');
+        }
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('user')->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect('/login')->with('success', 'Logged Out Successfully');
+    }
+}
