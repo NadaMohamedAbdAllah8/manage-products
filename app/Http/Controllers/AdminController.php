25e3cecf28e2bad605a357c4c8bc2c9044c058eb
nada <nada.mohamed@extech.com>
2022-04-25 00:43:28 +0200

    login admin, user

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AdminController extends Controller
+{
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        // Check email
+        $admin = Admin::where('name', $fields['name'])->first();
+
+        // Check password
+        if (!$admin || !Hash::check($fields['password'], $admin->password)) {
+            return back()->with('error', 'Bad credentials');
+        }
+
+        if (Auth::guard('admin')->attempt(['name' => request('name'),
+            'password' => request('password')])) {
+
+            return redirect()->route('task.create')
+                ->with('success', 'Logged In Successfully');
+
+        } else {
+            return back()->with('error', 'Bad credentials');
+        }
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect('/login')->with('success', 'Logged Out Successfully');
+    }
+}
---
+++ resources/views/admin/pages/auth/login.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title')
+{{ $title ??'Login-Admin' }}
+@endsection
+
+@section('content')
+
+<div class="formdiv">
+          <form action="{{route('login-admin')}}" method="POST">
+                    @csrf
+                    <h1>Admin Login</h1>
+                    <hr>
+
+                    <label for="username"><b>Admin Name</b></label>
+                    <input type="text" placeholder="Enter Admin Name" name="name" id="username" required>
+
+
+                    <label for="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" id="psw" required>
+
+                    <button type="submit" class="actionbtn primary">Sign in</button>
+          </form>
+</div>
+
+@endsection
