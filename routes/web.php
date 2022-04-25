a61b74925c5517fba7afbdbe634f9bc9c19dc282
nada <nada.mohamed@extech.com>
2022-04-25 19:29:51 +0200

    route groubs, category routes

--- routes/web.php
+++ routes/web.php
@@ -21,41 +21,43 @@ Route::get('login', function () {
     return view('user.pages.auth.login');
 });
 
-Route::post('/login', 'App\Http\Controllers\User\AuthController@login')
-    ->name('/login');
+Route::post('login', 'App\Http\Controllers\User\AuthController@login')
+    ->name('login');
 
 Route::get('admin/login', function () {
     return view('admin.pages.auth.login');
 });
 
 Route::post('/admin/login', 'App\Http\Controllers\Admin\AuthController@login')
-    ->name('admin/login');
+    ->name('admin.login');
 
 Route::group(['middleware' => ['auth:user']], function () {
 
-    Route::post('/user/logout', 'App\Http\Controllers\User\AuthController@logout');
+    Route::post('user.logout', 'App\Http\Controllers\User\AuthController@logout')
+        ->name('user.logout');
 
-    Route::get('/user/index', 'App\Http\Controllers\User\IndexController@index')
-        ->name('/user/index');
+    Route::get('user.index', 'App\Http\Controllers\User\IndexController@index')
+        ->name('user.index');
 
-    Route::group(['prefix' => 'user/products', 'as' => 'user/products.'], function () {
-
-        Route::get('', 'App\Http\Controllers\User\ProductController@index');
-
+    Route::group(['prefix' => 'user.product', 'as' => 'user.product.'], function () {
+        Route::get('index', 'App\Http\Controllers\User\ProductController@index')
+            ->name('index');
     });
-
 });
 
-Route::group(['middleware' => ['auth:admin']], function () {
+Route::group(['middleware' => ['auth:admin'], 'as' => 'admin.'], function () {
 
-    Route::post('/admin/logout', 'App\Http\Controllers\Admin\AuthController@logout');
+    Route::post('logout', 'App\Http\Controllers\Admin\AuthController@logout')
+        ->name('logout');
 
-    Route::get('/admin/index', 'App\Http\Controllers\Admin\IndexController@index')
-        ->name('/admin/index');
+    Route::get('index', 'App\Http\Controllers\Admin\IndexController@index')
+        ->name('index');
 
     Route::group(['prefix' => 'product', 'as' => 'product.'], function () {
-        Route::get('/', 'App\Http\Controllers\Admin\ProductController@index')
-            ->name('product');
+        Route::get('index', 'App\Http\Controllers\Admin\ProductController@index')
+            ->name('index');
     });
+
+    Route::resource('category', 'App\Http\Controllers\Admin\CategoryController');
 
 });
