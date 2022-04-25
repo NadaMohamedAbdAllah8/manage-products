eb0821303c9b91b165d97a47847fcb1869547994
nada <nada.mohamed@extech.com>
2022-04-25 18:56:40 +0200

    category product nullable realtion

--- database/migrations/2022_04_24_220202_create_products_table.php
+++ database/migrations/2022_04_24_220202_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
